90bcb772e215e75c01e61ffb36f3fb340e1cff74
Sara Beccerica <S1098790@studenti.univpm.it>
2022-01-01 17:12:19 +0100

    aggiunta metodo in classe apicall

--- src/main/java/CallToAPI/APICall.java
+++ src/main/java/CallToAPI/APICall.java
@@ -8,15 +8,22 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.ParseException;
 
+import BaseClasses.Currency;
+import BaseClasses.DailyData;
+
 public class APICall {
+	private static final int DAYSNUMBER = 5;
 	private static final String BASE_URL = "http://api.currencylayer.com/";
 	private static final String ENDPOINT = "historical";
 	private static final String CURRENCIES = "EUR,JPY,GBP,CHF,AUD";
+	private String[] currencies=CURRENCIES.split(",");
 	public String readFile() {
 		String key="";
 		try {
@@ -54,6 +61,38 @@ public class APICall {
 		}
 		return jObject;
 	}
-	
+	public ArrayList<DailyData> sendRequest() {
+		ArrayList<DailyData> allData = new ArrayList<>();
+		try {
+			for(int i=0;i<DAYSNUMBER;i++) {
+				Calendar date = Calendar.getInstance();
+				date.add(Calendar.DAY_OF_MONTH,-i);
+			 	String date1 = "" + date.get(Calendar.YEAR)+"-" + date.get(Calendar.MONTH) +"-"+ date.get(Calendar.DATE);
+			 	JSONObject obj = new JSONObject();
+				obj = sendLiveRequest(date1);
+			 	obj.remove("terms");
+			 	obj.remove("success");
+			 	obj.remove("privacy");
+			 	obj.remove("historical");
+				obj.remove("timestamp");
+				obj.remove("source");
+				ArrayList<Currency> dailyData = new ArrayList<>();
+				for(int j=0;j<5;j++) {
+					JSONObject quotes= new JSONObject();
+				 	quotes=(JSONObject)obj.get("quotes");
+				 	Currency e = new Currency("USD"+currencies[j],(double)quotes.get("USD"+currencies[j]));
+				 	dailyData.add(e);
+				 }
+				DailyData dataCurrencies = new DailyData();
+				dataCurrencies.setDate(date);
+				dataCurrencies.setCurrencies(dailyData);
+				allData.add(dataCurrencies);
+				Thread.sleep(2000);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return allData;
+	}
 
 }
