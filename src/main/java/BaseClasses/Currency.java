cc796b01806f1bd0d2884be1d98d1a3bf1542f0d
Attili Loris <S1097953@studenti.univpm.it>
2022-01-01 16:13:00 +0100

    creazione package BaseClasses e aggiunta classe Currency

---
+++ src/main/java/BaseClasses/Currency.java
@@ -0,0 +1,35 @@
+package BaseClasses;
+
+public class Currency {
+	private String name;
+	private double value;
+	
+	public Currency(String name, double value) {
+		this.name = name;
+		this.value = value;
+	}
+	public Currency() {
+		this.name="";
+		this.value=0;
+	}
+
+	public String toString() {
+		return "Currency [name=" + name + ", value=" + value + "]";
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	
+}
