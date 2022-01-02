73d128eda4528e9a87a5a68eb492b7f7bb158e46
Sara Beccerica <S1098790@studenti.univpm.it>
2022-01-02 10:15:53 +0100

    aggiunta prima rotta

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,5 +1,22 @@
 package Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import CallToAPI.ReadFile;
+import DataService.HistoricalData;
+
 public class Controller {
 
+	ReadFile call = new ReadFile();
+	HistoricalData data = new HistoricalData(call.readFile());
+
+	@RequestMapping(value ="/quotes",method = RequestMethod.GET)
+	public ResponseEntity<Object> requestAllCurrencies() {
+		data.ConvertData();
+		return new ResponseEntity<Object>(data.getAllCurrencies(),HttpStatus.OK);
+	}
+	
 }
