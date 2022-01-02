771304a525965c7d644d1e0755a5a10e0be897fd
Sara Beccerica <S1098790@studenti.univpm.it>
2022-01-02 10:18:04 +0100

    aggiunta seconda rotta

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,10 +1,12 @@
 package Controller;
 
+import java.util.Calendar;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import CallToAPI.ReadFile;
 import DataService.HistoricalData;
 
@@ -17,6 +19,16 @@ public class Controller {
 	public ResponseEntity<Object> requestAllCurrencies() {
 		data.ConvertData();
 		return new ResponseEntity<Object>(data.getAllCurrencies(),HttpStatus.OK);
+	}
+
+	@RequestMapping(value ="/dailyQuotes",method = RequestMethod.GET)
+	public ResponseEntity<Object> requestDailyCurrencies(@RequestParam("year") int year,
+														 @RequestParam("month") int month,
+														 @RequestParam("day") int day){
+		data.ConvertData();
+		Calendar date = Calendar.getInstance();
+		date.set(year, month, day);
+		return new ResponseEntity<Object>(data.getDailyCurrencies(date),HttpStatus.OK);
 	}
 	
 }
