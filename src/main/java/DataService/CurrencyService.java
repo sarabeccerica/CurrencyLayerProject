be89b46ecf012cc0200a526a2f8c0de4eb35bc16
Attili Loris <attililoris@gmail.com>
2022-01-02 09:59:01 +0100

    aggiunta interfaccia CurrencyService

---
+++ src/main/java/DataService/CurrencyService.java
@@ -0,0 +1,11 @@
+package DataService;
+
+import java.util.Calendar;
+import java.util.Vector;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public interface CurrencyService {
+
+}
