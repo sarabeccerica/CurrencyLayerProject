11ed666aa22657279b4a8e3514004df9194f55d6
Attili Loris <attililoris@gmail.com>
2022-01-02 10:04:29 +0100

    modifica interface

--- src/main/java/DataService/DataAnalysis.java
+++ src/main/java/DataService/DataAnalysis.java
@@ -1,16 +1,13 @@
 package DataService;
 
 import java.util.Calendar;
-
-import org.json.simple.JSONObject;
-
 import BaseClasses.Currency;
 
 //aggiungi eccezioni
 public interface DataAnalysis {
+	public abstract void ConvertData();
 	public abstract double CurrencyAverage(String currency);
 	public abstract double CurrencyVariance(String currency);
 	public abstract Currency DailyLower(Calendar date);
-	public abstract JSONObject getCurrencyStats(String currency);
 }
 
